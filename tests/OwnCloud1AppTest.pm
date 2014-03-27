92feb0a9d26c734a532c1bbbb0e0e5b21c3cc13c
Johannes Ernst <jernst@cldstr.com>
2014-03-27 21:54:11 +0000

    file upload now part of test

--- tests/OwnCloud1AppTest.pm
+++ tests/OwnCloud1AppTest.pm
@@ -23,7 +23,7 @@ use warnings;
 
 package OwnCloud1AppTest;
 
-use Data::Dumper;
+use IndieBox::Logging;
 use IndieBox::Testing::AppTest;
 
 # The states and transitions for this test
@@ -33,6 +33,66 @@ my $custPointValues = {
         'adminpass'  => 'specialpass'
 };
 my $filesAppRelativeUrl = '/index.php/apps/files';
+
+## Factored out upload routines.
+
+##
+# Perform an HTTP POST request uploading a file on the host on which the
+# application is being tested.
+# $c: TestContext
+# $relativeUrl: appended to the host's URL
+# $file: name of the file to be uploaded
+# $dir: the directory parameter
+# $requestToken: the form's request token
+# return: hash containing content and headers of the HTTP response
+sub httpUploadRelativeHost {
+    my $c            = shift;
+    my $relativeUrl  = shift;
+    my $file         = shift;
+    my $dir          = shift;
+    my $requestToken = shift;
+
+    my $url = 'http://' . $c->hostName . $relativeUrl;
+    my $response;
+
+    debug( 'Posting to url', $url );
+
+    my $cmd = $c->{curl};
+    $cmd .= " -F 'files[]=\@$file;filename=$file;type=text/plain'";
+    $cmd .= " -F 'requesttoken=$requestToken'";
+    $cmd .= " -F 'dir=$dir'";
+    $cmd .= " '$url'";
+
+    my $stdout;
+    my $stderr;
+    if( IndieBox::Utils::myexec( $cmd, undef, \$stdout, \$stderr )) {
+        $c->reportError( 'HTTP request failed:', $stderr );
+    }
+    return { 'content'     => $stdout,
+             'headers'     => $stderr,
+             'url'         => $url,
+             'file'        => $file };
+}
+
+##
+# Perform an HTTP POST request uploading a file on the application being tested,
+# appending to the context URL.
+# $c: TestContext
+# $relativeUrl: appended to the application's context URL
+# $file: name of the file to be uploaded
+# return: hash containing content and headers of the HTTP response
+sub httpUploadRelativeContext {
+    my $c            = shift;
+    my $relativeUrl  = shift;
+    my $file         = shift;
+    my $dir          = shift;
+    my $requestToken = shift;
+
+    return httpUploadRelativeHost( $c, $c->context() . $relativeUrl, $file, $dir, $requestToken );
+}
+
+##
+
 
 my $TEST = new IndieBox::Testing::AppTest(
     name                     => 'OwnCloud1AppTest',
@@ -82,6 +142,53 @@ my $TEST = new IndieBox::Testing::AppTest(
                             $c->reportError( 'Wrong logged-on page (logged-in user not shown)' );
                         }
                         					
+                        return 1;
+                    }
+            ),
+            new IndieBox::Testing::StateTransition(
+                    name       => 'upload-file',
+                    transition => sub {
+                        my $c = shift;
+
+                        # need to login first, and find requesttoken
+                        my $postData = {
+                            'user'            => $custPointValues->{'adminlogin'},
+                            'password'        => $custPointValues->{'adminpass'},
+                            'timezone-offset' => 0
+                        };
+                        
+                        my $response = $c->httpPostRelativeContext( '/', $postData );
+                        unless( $response->{headers} =~ m!HTTP/1.1 302 Found! ) {
+                            $c->reportError( 'Not HTTP Status 203', $response->{headers} );
+                        }
+
+                        $response = $c->httpGetRelativeContext( $filesAppRelativeUrl );
+                        unless( $response->{headers} =~ m!HTTP/1.1 200 OK! ) {
+                            $c->reportError( 'Not HTTP Status 200', $response->{headers} );
+                        }
+
+                        my $requestToken;
+                        if( $response->{content} =~ m!<head.*data-requesttoken="([0-9a-f]+)"! ) {
+                            $requestToken = $1;
+                        } else {
+                            $c->reportError( 'Cannot find request token', $response->{content} );
+                        }
+
+                        $response = httpUploadRelativeContext( $c, '/index.php/apps/files/ajax/upload.php', 'foo-testfile', '/', $requestToken );
+                        use Data::Dumper;
+                        print "XXX Response is " . Dumper( $response ) . "\n\n";
+                        unless( $response->{headers} =~ m!HTTP/1.1 200 OK! ) {
+                            $c->reportError( 'Not HTTP Status 200', $response->{headers} );
+                        }
+
+                        return 1;
+                    }
+            ),
+            new IndieBox::Testing::StateCheck(
+                    name  => 'file-uploaded',
+                    check => sub {
+                        my $c = shift;
+
                         return 1;
                     }
             )
